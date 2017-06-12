765c258ccdfbc202129d9c3604ab890776f0c140
Fernando Méndiz Carpi <hegemon70@gmail.com>
2017-06-12 13:32:42 +0200

    calculando las preferencias (no terminado)

--- app/src/main/java/com/example/masteraaa/botefmc/Ajustes.java
+++ app/src/main/java/com/example/masteraaa/botefmc/Ajustes.java
@@ -1,6 +1,7 @@
 package com.example.masteraaa.botefmc;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -56,7 +57,52 @@ public class Ajustes extends AppCompatActivity implements View.OnClickListener{
 
     }
 
+    private void aplicaCambios(SharedPreferences preferencias,boolean debug,boolean demo,boolean reset)
+    {
+        debug=(swiDebug.isChecked())?true:false;
+        demo=(swiDemo.isChecked())?true:false;
+        reset=(swiReset.isChecked())?true:false;
+        SharedPreferences.Editor editor=preferencias.edit();
+        editor.putBoolean("DEBUG",debug);
+        editor.putBoolean("DEMO",demo);
+        editor.putBoolean("RESET",reset);
+        editor.commit();
+    }
+    private void leePreferencias(SharedPreferences preferencias,boolean debug,boolean demo,boolean reset)
+    {
+        debug=preferencias.getBoolean("DEBUG",false);
+        demo=preferencias.getBoolean("DEMO",false);
+        reset=preferencias.getBoolean("RESET",false);
+    }
+    /*
+    private boolean hayCambios(boolean debug,boolean demo,boolean reset)
+    {   boolean cambioDemo=false;
+        boolean cambioDebug=false;
+        boolean cambioReset=false;
+        boolean cambios=false;
+        cambioDebug= swiDebug.isChecked()&& debug;
+        return cambios;
+    }
+    */
     private void calculaAjustes(){
-        
+        final SharedPreferences preferencias=getSharedPreferences("configuracion",MODE_PRIVATE);
+        boolean debug,demo,reset;
+        debug=false;
+        demo=false;
+        reset=false;
+
+        leePreferencias(preferencias,debug,demo,reset);
+        aplicaCambios(preferencias,debug,demo,reset);
+
+
+        if (debug){
+            String muestraBoolean="demo: ";
+            muestraBoolean += (demo)?"true ":"false ";
+            muestraBoolean+="debug:true ";
+            muestraBoolean="reset: ";
+            muestraBoolean += (demo)?"true ":"false";
+
+        }
+
     }
 }
